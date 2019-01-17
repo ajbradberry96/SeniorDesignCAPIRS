9df19c55936aa5cce6c2755849568a176d0a8c1d
TitanShadow12 <titanshadow12@gmail.com>
2019-01-16 23:17:40 -0600

    Rearranged noise_transform file to make it more modular

--- noise_transform.py
+++ noise_transform.py
@@ -9,49 +9,6 @@ import adv_example
 import forward_model 
 import plot_results
 
-def predict_and_plot(img, sess):
-    img_probs = forward_model.predict(img, sess)
-    plot_results.plot(img, img_probs)
-
-# setup
-tf.logging.set_verbosity(tf.logging.ERROR)
-sess = tf.InteractiveSession()
-
-# get image of cat
-img_path, _ = urlretrieve('http://www.anishathalye.com/media/2017/07/25/cat.jpg')
-img = PIL.Image.open(img_path)
-
-
-# check that classifier indeed identifies it as a cat
-#image_class_probs = forward_model.predict(img, sess)
-#plot_results.plot(img, image_class_probs)
-predict_and_plot(img, sess)
-
-# variables to use later, calculated from other functions
-# logits created when running inception
-# probs created wien running inception on image
-# image is what's being processed - tenserflow variable
-logits, probs, image = forward_model.get_logits_probs_image_tf(sess)
-
-# get our adversarial example and evaluate it
-#adv_img = adv_example.generate_adversarial_example(img,sess)
-adv_img = PIL.Image.open('adversarial_cat.png')
-
-
-# save image for later use (don't generate every time, for testing)
-#adv_img.save('adversarial_cat.png')
-
-#adv_class_probs = forward_model.predict(adv_img,sess)
-#plot_results.plot(adv_img, adv_class_probs)
-predict_and_plot(adv_img, sess)
-
-#robust_adv_img = adv_example.generate_adversarial_example(img, sess, mode="rot_robust")
-robust_adv_img = PIL.Image.open('robust_adversarial_cat.png')
-
-# save robust image for later use
-#robust_adv_img.save('robust_adversarial_cat.png')
-
-# no need to make a robust example
 def add_noise(image, noise_factor=1, noise_type="gauss"):
     noisy_image = None
     image_array = np.array(image)
@@ -99,30 +56,67 @@ def add_noise(image, noise_factor=1, noise_type="gauss"):
     
     return noisy_image
 
+
+"""everything below this is for standalone testing"""
+def predict_and_plot(img, sess):
+    img_probs = forward_model.predict(img, sess)
+    plot_results.plot(img, img_probs)
+
 def test_with_noise(images, noise_factor, noise_type, sess):
     for image in images:
         noised_image = add_noise(image, noise_factor=noise_factor,
                                  noise_type=noise_type)
         predict_and_plot(noised_image, sess)
+        
+def test_noise_transform:
+    """
+    Run the classifier against real image, adversarial image,
+    robust adversarial image, and run again with noise added.
+    """
+    # setup
+    tf.logging.set_verbosity(tf.logging.ERROR)
+    sess = tf.InteractiveSession()
 
-images = [img, adv_img, robust_adv_img]
-test_with_noise(images, 1, "speckle", sess)
+    # get image of cat
+    img_path, _ = urlretrieve('http://www.anishathalye.com/media/2017/07/25/cat.jpg')
+    img = PIL.Image.open(img_path)
 
-"""   
-# now add noise to the original image and see if it classifies correctly
-noised_image = add_noise(img)
-predict_and_plot(noised_image, sess)
 
-# now check adversarial, see if it's fooled
-noised_adv_img = add_noise(adv_img)
-predict_and_plot(noised_adv_img, sess)
+    # check that classifier indeed identifies it as a cat
+    #image_class_probs = forward_model.predict(img, sess)
+    #plot_results.plot(img, image_class_probs)
+    predict_and_plot(img, sess)
 
-# now check robust
-noised_robust = add_noise(robust_adv_img, noise_factor=1)
-predict_and_plot(noised_robust, sess)
-"""
+    # variables to use later, calculated from other functions
+    # logits created when running inception
+    # probs created wien running inception on image
+    # image is what's being processed - tenserflow variable
+    logits, probs, image = forward_model.get_logits_probs_image_tf(sess)
 
-# test effect of multiple different noise factors
-#for i in np.linspace(0,1,11):
-#    print(f"Testing noise_factor of {i}")
-#    test_with_noise(images, i, "speckle", sess)
+    # get our adversarial example and evaluate it
+    #adv_img = adv_example.generate_adversarial_example(img,sess)
+    adv_img = PIL.Image.open('adversarial_cat.png')
+
+
+    # save image for later use (don't generate every time, for testing)
+    #adv_img.save('adversarial_cat.png')
+
+    #adv_class_probs = forward_model.predict(adv_img,sess)
+    #plot_results.plot(adv_img, adv_class_probs)
+    predict_and_plot(adv_img, sess)
+
+    #robust_adv_img = adv_example.generate_adversarial_example(img, sess, mode="rot_robust")
+    robust_adv_img = PIL.Image.open('robust_adversarial_cat.png')
+
+    # save robust image for later use
+    #robust_adv_img.save('robust_adversarial_cat.png')
+
+    images = [img, adv_img, robust_adv_img]
+    test_with_noise(images, 2, "saltpepper", sess)
+
+    # test effect of multiple different noise factors
+    #for i in np.linspace(0,1,11):
+    #    print(f"Testing noise_factor of {i}")
+    #    test_with_noise(images, i, "speckle", sess)
+
+    
