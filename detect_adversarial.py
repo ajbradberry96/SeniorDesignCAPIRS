ea1cfc4e6e4700bb4737942ed66f73606280218a
Chris Rodriguez <chrizrodz@gmail.com>
2019-03-12 13:49:53 -0500

    Made the code more readable and presentation ready
    Code is now easier to run, and the display messages make sense. Also runs everything faster, so we can do the presentation without delays.

--- detect_adversarial.py
+++ detect_adversarial.py
@@ -1,6 +1,7 @@
 from scipy import spatial
 import numpy as np
 import forward_model
+import plot_results
 import image_processing
 import os
 
@@ -18,23 +19,31 @@ def detect(img):
 
 	for i in range(3):
 		col_img = image_processing.color_shift(img)
-		t_vec = list(forward_model.predict(col_img))
-		transform_vectors.append(t_vec)
+		t_vec = forward_model.predict(col_img)
+		transform_vectors.append(list(t_vec))
+		cosine_diff = spatial.distance.cosine(orig_vector, t_vec)
+		plot_results.plot(col_img,t_vec,cos_diff=cosine_diff)
 
 	for i in range(3):
 		sat_img = image_processing.saturate_mod(img)
-		t_vec = list(forward_model.predict(sat_img))
-		transform_vectors.append(t_vec)
+		t_vec = forward_model.predict(sat_img)
+		transform_vectors.append(list(t_vec))
+		cosine_diff = spatial.distance.cosine(orig_vector, t_vec)
+		plot_results.plot(sat_img,t_vec,cos_diff=cosine_diff)
 
 	for i in range(3):
 		noise_img = image_processing.add_noise(img)
-		t_vec = list(forward_model.predict(noise_img))
-		transform_vectors.append(t_vec)
+		t_vec = forward_model.predict(noise_img)
+		transform_vectors.append(list(t_vec))
+		cosine_diff = spatial.distance.cosine(orig_vector, t_vec)
+		plot_results.plot(noise_img,t_vec,cos_diff=cosine_diff)
 
 	for i in range(3):
 		warp_img = image_processing.rand_warp(img)
-		t_vec = list(forward_model.predict(warp_img))
-		transform_vectors.append(t_vec)
+		t_vec = forward_model.predict(warp_img)
+		transform_vectors.append(list(t_vec))
+		cosine_diff = spatial.distance.cosine(orig_vector, t_vec)
+		plot_results.plot(warp_img,t_vec,cos_diff=cosine_diff)
 
 	average_trans_vector = list(np.average(transform_vectors, axis=0))
 	cosine_diff = spatial.distance.cosine(orig_vector, average_trans_vector)
@@ -173,5 +182,33 @@ def detect_test(imgs, names, separate_advs=True):
 			f.write(img_name+vec_name+"," + str(cosine_diff) + "\n")
 
 
+
+if __name__ == "__main__":
+	import tensorflow as tf
+	import PIL
+	tf.logging.set_verbosity(tf.logging.ERROR)
+	sess = tf.InteractiveSession()
+
+	img = PIL.Image.open("media/norm_img.png")
+	adv_img = PIL.Image.open("media/adv_img.png")
+
+	#img = PIL.Image.open("media/cat.png")
+	#adv_img = PIL.Image.open("media/adversarial_cat.png")
+
+	forward_model.init(sess)
+
+	print("NORMAL IMAGE: ")
+	detect(img)
+	print()
+	print()
+
+	plot_results.plt.show()
+
+	print("ADVERSARIAL IMAGE: ")
+	detect(adv_img)
+	print()
+	print()
+
+	plot_results.plt.show()
 
 
--- plot_results.py
+++ plot_results.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import forward_model
 
 
-def plot(img, probs):
+def plot(img, probs, cos_diff=-1):
 	fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 8))
 	fig.sca(ax1)
 	ax1.imshow(img)
@@ -12,10 +12,33 @@ def plot(img, probs):
 	topk = list(probs.argsort()[-10:][::-1])
 	topprobs = probs[topk]
 	barlist = ax2.bar(range(10), topprobs)
+	if cos_diff != -1:
+		plt.text(0,-100,"Cosine Distance: " + str(round(cos_diff,3)))
 	plt.sca(ax2)
 	plt.ylim([0, 1.1])
 	plt.xticks(range(10),
 			[imagenet_labels[i][:15] for i in topk],
 			rotation='vertical')
 	fig.subplots_adjust(bottom=0.2)
+
+if __name__ == "__main__":
+	import tensorflow as tf
+	import PIL
+	tf.logging.set_verbosity(tf.logging.ERROR)
+	sess = tf.InteractiveSession()
+
+	img = PIL.Image.open("media/norm_img.png")
+	adv_img = PIL.Image.open("media/adv_img.png")
+
+	#img = PIL.Image.open("media/cat.png")
+	#adv_img = PIL.Image.open("media/adversarial_cat.png")
+
+	forward_model.init(sess)
+
+	norm_probs = forward_model.predict(img)
+	adv_probs = forward_model.predict(adv_img)
+
+	plot(img,norm_probs)
+	plot(adv_img,adv_probs)
 	plt.show()
+
